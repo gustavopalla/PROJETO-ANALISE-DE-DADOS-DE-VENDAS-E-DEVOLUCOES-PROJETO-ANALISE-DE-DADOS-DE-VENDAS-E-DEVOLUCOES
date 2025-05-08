8bd3486394df25760a4533112cab7e3f2b96a2f1
Gustavo Palla <160782184+gustavopalla@users.noreply.github.com>
2025-05-07 21:13:39 -0300

    teste commit
    teste

--- PROJETO ANALISE DE DADOS DE VENDAS E DEVOLUÇÕES/src/init.py
+++ PROJETO ANALISE DE DADOS DE VENDAS E DEVOLUÇÕES/src/init.py
@@ -46,3 +46,5 @@ grafico_produtos_devolvidos = px.bar(tabela_devolucao, x=tabela_devolucao.index,
 grafico_faturamento_loja.show()
 grafico_produtos_devolvidos.show()
 grafico_produtos_vendidos.show()
+
+#testando commit
